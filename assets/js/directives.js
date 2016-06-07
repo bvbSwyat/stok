00dee6994826188f175f3a2cf79bae6555785cd0
swyat <swyatyxa@gmail.com>
2016-06-07 16:09:59 +0300

    fixed 15

--- assets/js/directives.js
+++ assets/js/directives.js
@@ -2,7 +2,11 @@
  * Created by swyat on 19.04.15.
  */
 var NgApp = angular.module("NgApp", [
-
+    "ui.bootstrap",
+    "ngRoute",
+    "LocalStorageModule",
+    "yaMap",
+    "ngResource"
 ]);
 
 NgApp.controller('MainController', function () {
